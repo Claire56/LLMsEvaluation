e0e7f5b84174170b3e12cebe34eb87198630b29c
Claire Kim <kclamu26@gmail.com>
2025-12-08 20:09:07 -0800

    Create __init__.py

---
+++ dashboard/__init__.py
@@ -0,0 +1,8 @@
+"""
+Dashboard visualization for evaluation results.
+"""
+
+from .visualizer import create_dashboard, generate_summary_stats
+
+__all__ = ['create_dashboard', 'generate_summary_stats']
+
